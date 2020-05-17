7ad437ff4f158867e560c210030479d34f717833
j-o-lantern0422 <j.o.lantern0422@gmail.com>
2020-05-17 17:47:56 +0900

    change csvcop start command

--- lib/csvcop/cli.rb
+++ lib/csvcop/cli.rb
@@ -4,9 +4,9 @@ require "csv"
 
 module CsvCop
   class Cli < Thor
-    desc "csvcop {CSV File Path} {options}", "check csv file"
+    desc "run {CSV File Path} {options}", "check csv file"
     option "config", aliases: "-c", type: :string
-    def csvcop(csv_file_path)
+    def start(csv_file_path)
       config = CsvCop::Config.new(options[:config]).read
       csv = CSV.table(csv_file_path)
       cop = CsvCop::Cop::Cop.new(csv, config)
